3e89ea4ab0b276a138ca630dd9abb4cef3dd23a5
sajib-hosen <sajib.201h@gmail.com>
2023-12-10 23:26:00 +0600

    working on dubly link list

--- doubly_linked_list_02.py
+++ doubly_linked_list_02.py
@@ -4,8 +4,23 @@
 # email: sajib.201h@gamil.com
 
 # DUBLY LINKED_LIST 
+# Advantages of Doubly Linked List over the singly linked list:
+#    A DLL can be traversed in both forward and backward directions. 
+#    The delete operation in DLL is more efficient if a pointer to the node to be deleted is given. 
+#    We can quickly insert a new node before a given node. 
+#    In a singly linked list, to delete a node, a pointer to the previous node is needed. To get this previous node, sometimes the list is traversed. In DLL, we can get the previous node using the previous pointer. 
 
+# Disadvantages of Doubly Linked List over the singly linked list:
+#    Every node of DLL Requires extra space for a previous pointer. It is possible to implement DLL with a single pointer though (See this and this). 
+#    All operations require an extra pointer previous to be maintained. For example, in insertion, we need to modify previous pointers together with the next pointers. For example in the following functions for insertions at different positions, we need 1 or 2 extra steps to set the previous pointer.
 
+# Applications of Doubly Linked List:
+#    It is used by web browsers for backward and forward navigation of web pages 
+#    LRU ( Least Recently Used ) / MRU ( Most Recently Used ) Cache are constructed using Doubly Linked Lists. 
+#    Used by various applications to maintain undo and redo functionalities. 
+#    In Operating Systems, a doubly linked list is maintained by thread scheduler to keep track of processes that are being executed at that time.
+
+# Ref >> https://www.geeksforgeeks.org/data-structures/linked-list/doubly-linked-list/ 
 
 class Node:
     def __init__(self, data) -> None:  # Function to initialize the node object
@@ -17,6 +32,7 @@ class Node:
 class DublyLinkedList:
     def __init__(self) -> None:
         self.head = None
+        self.tail = None
 
     #print the list
     def printF(self):
